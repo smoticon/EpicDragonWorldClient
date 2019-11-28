08a91290c71dd035401d7fcc566261399801b43d
PantelisAndrianakis <pantelisandrianakis@gmail.com>
2019-11-28 18:01:38 +0200

    Store Item and NPC data in files.

--- Assets/Scripts/Data/ItemData.cs
+++ Assets/Scripts/Data/ItemData.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEngine;
 
 /**
@@ -15,11 +17,50 @@ public class ItemData : MonoBehaviour
     private void Start()
     {
         Instance = this;
-        ITEMS.Add(1, new ItemHolder(1, ItemSlot.CHEST, ItemType.EQUIP, "Test Shirt", "Tank tops make great first items.", "MaleShirt2", "FemaleShirt2", -1, Vector3.zero, Vector3.zero, new Quaternion(), new Quaternion(), Vector3.one, Vector3.one, false, false, 0, 0, 0, 0));
-        ITEMS.Add(2, new ItemHolder(2, ItemSlot.LEGS, ItemType.EQUIP, "Test Pants", "Some people call them jeans, others... just pants.", "MaleJeans", "FemalePants1", -1, Vector3.zero, Vector3.zero, new Quaternion(), new Quaternion(), Vector3.one, Vector3.one, false, false, 0, 0, 0, 0));
-        ITEMS.Add(3, new ItemHolder(3, ItemSlot.FEET, ItemType.EQUIP, "Test Shoes", "Definitely not tall shoes.", "TallShoes_Black_Recipe", "FemaleTallShoes_Black", -1, Vector3.zero, Vector3.zero, new Quaternion(), new Quaternion(), Vector3.one, Vector3.one, false, false, 0, 0, 0, 0));
-        ITEMS.Add(4, new ItemHolder(4, ItemSlot.RIGHT_HAND, ItemType.EQUIP, "Test Sword Right", "Sword of thunder.", "", "", 0, new Vector3(-0.09f, -0.24f, -0.09f), new Vector3(-0.09f, -0.24f, -0.09f), Quaternion.Euler(12.738f, -5.85f, -0f), Quaternion.Euler(12.738f, -5.85f, -0f), Vector3.one, Vector3.one, false, false, 0, 0, 0, 0));
-        ITEMS.Add(5, new ItemHolder(5, ItemSlot.LEFT_HAND, ItemType.EQUIP, "Test Sword Left", "Sword of lightning.", "", "", 0, new Vector3(-0.1f, 0.3f, -0.102f), new Vector3(-0.1f, 0.3f, -0.102f), Quaternion.Euler(-12.738f, -5.85f, 182.189f), Quaternion.Euler(-12.738f, -5.85f, 182.189f), Vector3.one, Vector3.one, false, false, 0, 0, 0, 0));
+
+        TextAsset data = Resources.Load("data/ItemData") as TextAsset;
+        string[] lines = Regex.Split(data.text, "\r\n|\n\r|\n|\r");
+        foreach (string line in lines)
+        {
+            if (line.StartsWith("#"))
+            {
+                continue;
+            }
+            string[] values = Regex.Split(line, ";");
+            if (values.Length < 20)
+            {
+                continue;
+            }
+
+            int itemId = int.Parse(values[0]);
+            ItemSlot itemSlot = (ItemSlot)Enum.Parse(typeof(ItemSlot), values[1]);
+            ItemType itemType = (ItemType)Enum.Parse(typeof(ItemType), values[2]);
+            string name = values[3];
+            string description = values[4];
+            string recipeMale = values[5];
+            string recipeFemale = values[6];
+            int prefabId = int.Parse(values[7]);
+            string[] positionMaleSplit = Regex.Split(values[8], ",");
+            Vector3 positionMale = new Vector3(float.Parse(positionMaleSplit[0]), float.Parse(positionMaleSplit[1]), float.Parse(positionMaleSplit[2]));
+            string[] positionFemaleSplit = Regex.Split(values[9], ",");
+            Vector3 positionFemale = new Vector3(float.Parse(positionFemaleSplit[0]), float.Parse(positionFemaleSplit[1]), float.Parse(positionFemaleSplit[2]));
+            string[] rotationMaleSplit = Regex.Split(values[10], ",");
+            Quaternion rotationMale = Quaternion.Euler(float.Parse(rotationMaleSplit[0]), float.Parse(rotationMaleSplit[1]), float.Parse(rotationMaleSplit[2]));
+            string[] rotationFemaleSplit = Regex.Split(values[11], ",");
+            Quaternion rotationFemale = Quaternion.Euler(float.Parse(rotationFemaleSplit[0]), float.Parse(rotationFemaleSplit[1]), float.Parse(rotationFemaleSplit[2]));
+            string[] scaleMaleSplit = Regex.Split(values[12], ",");
+            Vector3 scaleMale = new Vector3(float.Parse(scaleMaleSplit[0]), float.Parse(scaleMaleSplit[1]), float.Parse(scaleMaleSplit[2]));
+            string[] scaleFemaleSplit = Regex.Split(values[13], ",");
+            Vector3 scaleFemale = new Vector3(float.Parse(scaleFemaleSplit[0]), float.Parse(scaleFemaleSplit[1]), float.Parse(scaleFemaleSplit[2]));
+            bool stackable = bool.Parse(values[14]);
+            bool tradable = bool.Parse(values[15]);
+            int stamina = int.Parse(values[16]);
+            int strength = int.Parse(values[17]);
+            int dexterity = int.Parse(values[18]);
+            int intelect = int.Parse(values[19]);
+
+            ITEMS.Add(itemId, new ItemHolder(itemId, itemSlot, itemType, name, description, recipeMale, recipeFemale, prefabId, positionMale, positionFemale, rotationMale, rotationFemale, scaleMale, scaleFemale, stackable, tradable, stamina, strength, dexterity, intelect));
+        }
     }
 
     public static ItemHolder GetItem(int id)
--- Assets/Scripts/Data/NpcData.cs
+++ Assets/Scripts/Data/NpcData.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEngine;
 
 /**
@@ -15,17 +16,40 @@ public class NpcData : MonoBehaviour
     {
         Instance = this;
 
-        //TODO: Create NPC costructor.
-        CharacterDataHolder testNpc = new CharacterDataHolder();
-        testNpc.SetName("Test NPC");
-        testNpc.SetRace(1);
-        testNpc.SetHeight(0.5f);
-        testNpc.SetBelly(0.5f);
-        testNpc.SetHairType(3);
-        testNpc.SetHairColor(15150895);
-        testNpc.SetSkinColor(15847869);
-        testNpc.SetEyeColor(15150895);
-        NPCS.Add(1, testNpc);
+        TextAsset data = Resources.Load("data/NpcData") as TextAsset;
+        string[] lines = Regex.Split(data.text, "\r\n|\n\r|\n|\r");
+        foreach (string line in lines)
+        {
+            if (line.StartsWith("#"))
+            {
+                continue;
+            }
+            string[] values = Regex.Split(line, ";");
+            if (values.Length < 17)
+            {
+                continue;
+            }
+
+            CharacterDataHolder holder = new CharacterDataHolder();
+            holder.SetName(values[1]);
+            holder.SetRace(byte.Parse(values[2]));
+            holder.SetHeight(float.Parse(values[3]));
+            holder.SetBelly(float.Parse(values[4]));
+            holder.SetHairType(int.Parse(values[5]));
+            holder.SetHairColor(int.Parse(values[6]));
+            holder.SetSkinColor(int.Parse(values[7]));
+            holder.SetEyeColor(int.Parse(values[8]));
+            holder.SetHeadItem(int.Parse(values[9]));
+            holder.SetChestItem(int.Parse(values[10]));
+            holder.SetLegsItem(int.Parse(values[11]));
+            holder.SetHandsItem(int.Parse(values[12]));
+            holder.SetFeetItem(int.Parse(values[13]));
+            holder.SetRightHandItem(int.Parse(values[14]));
+            holder.SetLeftHandItem(int.Parse(values[15]));
+            holder.SetHp(int.Parse(values[16]));
+
+            NPCS.Add(int.Parse(values[0]), holder);
+        }
     }
 
     public static CharacterDataHolder GetNpc(int id)
